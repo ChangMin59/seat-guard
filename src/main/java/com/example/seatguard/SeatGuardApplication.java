baa6e85a3eef64b8010c1fbb068ac18570361634
ChangMin59 <dy2ghks@gmail.com>
2026-04-24 14:23:44 +0900

    init: spring boot project setup

---
+++ src/main/java/com/example/seatguard/SeatGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.seatguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seatguard/SeatGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seatguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
