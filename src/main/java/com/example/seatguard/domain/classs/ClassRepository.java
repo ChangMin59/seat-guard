7719838bdfe5d90abb9d4dfcbfa49087eccdd437
ChangMin59 <dy2ghks@gmail.com>
2026-04-24 16:13:23 +0900

    feat: add JPA repositories for class and enrollment

---
+++ src/main/java/com/example/seatguard/domain/classs/ClassRepository.java
@@ -0,0 +1,6 @@
+package com.example.seatguard.domain.classs;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassRepository extends JpaRepository<Class, Long> {
+}
