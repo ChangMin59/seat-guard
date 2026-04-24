1f48b588f648c4582325a45df3d38aa2d2081741
ChangMin59 <dy2ghks@gmail.com>
2026-04-24 16:01:57 +0900

    feat: add enrollment entity

---
+++ src/main/java/com/example/seatguard/domain/enrollment/Enrollment.java
@@ -0,0 +1,30 @@
+package com.example.seatguard.domain.enrollment;
+
+import com.example.seatguard.domain.classs.Class;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId; // 사용자 ID (간단 처리)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id")
+    private Class clazz; // 어떤 강의인지
+
+    @Enumerated(EnumType.STRING)
+    private EnrollmentStatus status;
+
+    private LocalDateTime createdAt;
+}
